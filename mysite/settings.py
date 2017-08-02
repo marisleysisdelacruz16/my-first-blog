32a215b2b2153d2a5f5ff69c1b4ce8073ab4304a
marisleysisdelacruz16 <marisleidy06@gmail.com>
2017-08-02 14:35:28 -0400

    commiting stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*jsg#k)o&r5m9g52sqd8nw0lckil_x=rqxldzjibt5=+vtje=e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '<marisleysisdelacruz16>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'marisleysisdelacruz16.pythonanywhere.com']
+
 
 
 # Application definition
